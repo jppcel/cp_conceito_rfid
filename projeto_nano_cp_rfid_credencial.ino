d5348693abca744035b4361a6ae20a3c2c8f8604
João Paulo Polles <joaopaulopolles@gmail.com>
2018-02-03 05:12:15 -0200

    alterações para o arduino leonardo

--- projeto_nano_cp_rfid_credencial.ino
+++ projeto_nano_cp_rfid_credencial.ino
@@ -1,5 +1,3 @@
-
-
 /*
  * 
  * All the resources for this project: http://randomnerdtutorials.com/
@@ -8,7 +6,8 @@
  * Created by FILIPEFLOP
  * 
  */
- 
+
+#include<Keyboard.h>
 #include <SPI.h>
 #include <MFRC522.h>
  
@@ -16,24 +15,19 @@
 #define RST_PIN 9
 #define BUZ 8
 
-#define RED 3
-#define BLUE 4
-#define GREEN 2
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
  
 void setup() {
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
+  delay(1000);
   Serial.println("Approximate your card to the reader...");
   Serial.println();
 
   pinMode(BUZ, OUTPUT);
-  pinMode(BLUE, OUTPUT);
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  digitalWrite(BLUE, HIGH);
-  
+  Keyboard.begin();
+  delay(1000);
 
 }
 void loop() 
@@ -62,63 +56,14 @@ void loop()
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "64 7E 64 EB") //change here the UID of the card/cards that you want to give access
-  {
-    Serial.println("Authorized access");
-    Serial.println();
-    setColor(1);
-    tone(BUZ,2000);    
-    // Espera um tempo para Desativar
-    delay(1000);
-    //Desativa o buzzer
-    noTone(BUZ); 
-    delay(2000);
-    setColor(0);
-  }
- 
- else   {
-    Serial.println(" Access denied");
-    setColor(2);
-    tone(BUZ,200);    
-    // Espera um tempo para Desativar
-    delay(200);
-    setColor(3);
-    tone(BUZ,300);  
-    delay(200);
-    setColor(2);
-    noTone(BUZ);   
-    // Espera um tempo para Desativar
-    delay(200);
-    setColor(2);
-    tone(BUZ,200);    
-    delay(200);
-    setColor(3);
-    noTone(BUZ); 
-    // Espera um tempo para Desativar
-    delay(200);
-    setColor(2);
-    tone(BUZ,300);    
-    delay(200);
-    noTone(BUZ); 
-    delay(2000);
-    setColor(0);
-  }
+  Keyboard.println(content.substring(1));
+  Keyboard.press(KEY_RETURN);
+  Keyboard.release(KEY_RETURN);
+  tone(BUZ,1000);    
+  // Espera um tempo para Desativar
+  delay(100);
+  //Desativa o buzzer
+  noTone(BUZ); 
+  delay(1000);
 } 
-
-void setColor(int color){
-  digitalWrite(RED, LOW);
-  digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, LOW);
-  switch(color){
-    case 0:
-      digitalWrite(BLUE, HIGH);
-      break;
-    case 1:
-      digitalWrite(GREEN, HIGH);
-      break;
-    case 2:
-      digitalWrite(RED, HIGH);
-      break;
-  }
-}
 
